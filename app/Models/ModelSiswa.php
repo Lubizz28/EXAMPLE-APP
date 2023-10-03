c2cb0ba992b25355b37aa16825b2c800281d81be
Lubizz28 <hadi.lubizz@gmail.com>
2023-10-03 13:46:58 +0700

    first commit

---
+++ app/Models/ModelSiswa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelSiswa extends Model
+{
+    use HasFactory;
+    protected $table = 'siswa';
+    protected $primarykey = 'nis';
+
+    public $timestamps = false;
+}
